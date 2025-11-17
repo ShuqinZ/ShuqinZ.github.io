5525ec66efc6c6086270ca13e6408972397d41e8
Shuqin <shuqinzhu37@gmail.com>
2025-11-17 15:28:17 -0800

    update for qual exam

--- src/components/Template/SideBar.tsx
+++ src/components/Template/SideBar.tsx
@@ -15,12 +15,12 @@ const SideBar: React.FC = () => {
     <section id="sidebar">
       <section id="intro">
         <Link href="/" className="logo">
-          <Image src="/images/me.jpg" alt="Michael D'Angelo" width={200} height={200} priority />
+          <Image src="/images/me.jpg" alt="Shuqin Zhu" width={200} height={200} priority />
         </Link>
         <header>
-          <h2>Michael D&apos;Angelo</h2>
+          <h2>Shuqin Zhu</h2>
           <p>
-            <a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a>
+            <a href="shuqin:shuqinzh@usc.edu">shuqinzh@usc.edu</a>
           </p>
         </header>
       </section>
@@ -28,12 +28,12 @@ const SideBar: React.FC = () => {
       <section className="blurb">
         <h2>About</h2>
         <p>
-          Hi, I&apos;m Michael. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a>{' '}
-          graduate, YC alumnus, and the co-founder and CTO of{' '}
-          <a href="https://promptfoo.dev">Promptfoo</a>. Previously, I was VP of Engineering at{' '}
-          <a href="https://usesmileid.com">SmileID</a>, co-founder and CTO of{' '}
-          <a href="https://arthena.com">Arthena</a>, and co-founded{' '}
-          <a href="https://matroid.com">Matroid</a>.
+          Hi, I’m Shuqin Zhu. 
+          I am a third year Computer Science Ph.D. Candidate at University of Southern California, advised by Professor Shahram Ghandeharizadeh.
+          I am affiliated with <a href="https://www.flslab.org/">FLS (Flying Light Specks)Lab </a>,
+          My research focus on UAV(Drones), robotics, haptics, and multi-agent drone systems. 
+
+
         </p>
         <ul className="actions">
           <li>
